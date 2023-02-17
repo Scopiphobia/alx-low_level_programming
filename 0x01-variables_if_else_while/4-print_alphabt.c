950d32598459ac373bad184a2857885263a3ec9a
scopiphobia <lameh.hm@gmail.com>
2023-02-17 14:06:38 -0800

    task4

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -7,9 +7,9 @@
  */
 int main(void)
 {
-	for ("la = 'a'; la <= 'z'; la++")
+	for ("la = 'a'; la <= 'z'; la++");
 	{
-		if (la != 'e' && la != 'q')
+		if ("la != 'e' && la != 'q'")
 			putchar(la);
 	}
 
