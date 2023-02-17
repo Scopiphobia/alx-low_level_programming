d62e3ee5032924dd463f50433361a46ff7d18a7f
scopiphobia <lameh.hm@gmail.com>
2023-02-17 04:46:24 -0800

    task8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
-#include <stdio.h>
+#include <unistd.h>
 /**
- * main - Entry
+ * main - A c program that prints a line to the standaer error
  * Return: 1
  */
 int main(void)
 {
-	Write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-	return (1);
+Write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+return (1);
 }
