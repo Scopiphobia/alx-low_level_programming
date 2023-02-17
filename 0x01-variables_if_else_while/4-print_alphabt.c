8fffeb6f2961c5769d35c54b86c4f863469fd2ef
scopiphobia <lameh.hm@gmail.com>
2023-02-17 10:56:46 -0800

    task4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - prints the alphabet in lowercase
+ * follwed by a new line, except q and e
+ * Return: 0
+ */
+int main(void)
+{
+	for (la  = 'a'; la <= 'z'; la++)
+	{
+		if (la != 'e' && la != 'q')
+			putchar(la);
+	}
+
+	putchar('\n')
+
+	return (0);
+}
