4800aba3ef9a658267a5a99c74b08bde6a22b35a
scopiphobia <lameh.hm@gmail.com>
2023-02-22 10:22:37 -0800

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,10 @@
-#include '"main.h"'
+#include <main.h>
 /**
  * main - Entry point
  * Description: 'prints _putchar to stdout'
  * Return: always 0
  */
-int main(void)
+int _putchar(char)
 {
 	_putchar('_');
 	_putchar('p');
