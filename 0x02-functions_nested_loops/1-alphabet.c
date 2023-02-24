53f6a572c18d4e7f6c93963f72447a85f14a4881
scopiphobia <lameh.hm@gmail.com>
2023-02-24 04:47:22 -0800

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,8 +5,15 @@
  *
  * Return: Always 0.
  */
-int main(void)
+int print_alphabet(void)
 {
-	print_alphabet();
+	int i;
+
+	for (i = 97; i <= 122; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+
 	return (0);
 }
